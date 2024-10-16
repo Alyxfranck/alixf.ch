57ecb5afa2820ddc8aa3699ac0be9bd18641cc34
Alix Franck <130584063+Alyxfranck@users.noreply.github.com>
2024-10-16 22:16:28 +0200

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,6 +10,7 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Blog</h1>
+      <MandelbulbSketch />
       <BlogPosts />
     </section>
   )
