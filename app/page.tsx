9d0c8111aeaf482f8de404abce59b2c02f640e1b
Alix Franck <130584063+Alyxfranck@users.noreply.github.com>
2024-10-16 19:32:45 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from 'app/components/posts';
 import MandelbulbSketch from 'app/components/MandelbulbSketch';
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Hello, Iam Alix Franck
+        Hello, I am Alix Franck
       </h1>
       <p className="mb-4">
-        I’m a developer blending creativity with efficiency. As co-founder of Buillt.ch and Swiss Founders Lab, I modernize websites and bring innovative business ideas to life using cutting-edge technology.
+        I’m a developer blending creativity with efficiency. As co-founder of{' '}
+        <a href="https://buillt.ch" target="_blank" rel="noopener noreferrer">Buillt.ch</a> and{' '}
+        <a href="https://www.swissfounderslab.com/" target="_blank" rel="noopener noreferrer">Swiss Founders Lab</a>, I modernize websites and bring innovative business ideas to life using cutting-edge technology.
       </p>
       <p className="mb-4">
         Recent projects include a 3D visualization using live data and the development of Buillt Docs, a real-time collaborative editing tool. Whether I'm coding or building apps, dark mode is a must for me, as it keeps me focused during long sessions while minimizing eye strain.
@@ -19,5 +20,5 @@ export default function Page() {
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
