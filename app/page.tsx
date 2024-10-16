691fc5b0592f7e8ea07d60920542ca26d900b7e4
Alix Franck <130584063+Alyxfranck@users.noreply.github.com>
2024-10-16 19:23:04 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         Hello, Iam Alix Franck
       </h1>
       <p className="mb-4">
-       I’m a developer and tech enthusiast, combining creative problem-solving with efficiency. As co-founder of Buillt.ch and Swiss Founders Lab, I modernize websites and implement innovative business ideas with modern technology.
+        I’m a developer blending creativity with efficiency. As co-founder of Buillt.ch and Swiss Founders Lab, I modernize websites and bring innovative business ideas to life using cutting-edge technology.
       </p>
       <p className="mb-4">
         Recent projects include a 3D visualization using live data and the development of Buillt Docs, a real-time collaborative editing tool. Whether I'm coding or building apps, dark mode is a must for me, as it keeps me focused during long sessions while minimizing eye strain.
