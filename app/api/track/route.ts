152e23f2d9908aef5e2c20d9299028e912af48d0
Alixfranck <alufranck@gmail.com>
2025-02-12 16:41:50 +0100

    api/track/route.ts

--- app/api/track/route.ts
+++ app/api/track/route.ts
@@ -1,42 +1,24 @@
-// /pages/api/track.js
-import { NextApiRequest, NextApiResponse } from 'next';
-import fetch from 'node-fetch';
+import { NextRequest, NextResponse } from "next/server";
 
-interface Data {
-  // Define the structure of the data object here
-}
+export async function POST(req: NextRequest) {
+  try {
+    const data = await req.json();
 
-interface ErrorResponse {
-  message: string;
-  error?: string;
-}
+    // Forward data to an external application for processing
+    const externalResponse = await fetch("https://data-bucket.vercel.app/api/process", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-interface SuccessResponse {
-  status: string;
-}
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse<SuccessResponse | ErrorResponse>): Promise<void> {
-  if (req.method === 'POST') {
-    try {
-      const data: Data = req.body;
-      
-      const externalResponse = await fetch('https://data-bucket.vercel.app/api/process', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (!externalResponse.ok) {
-        throw new Error(`Failed to forward data: ${externalResponse.statusText}`);
-      }
-
-      res.status(200).json({ status: 'Tracked and forwarded successfully' });
-    } catch (error) {
-      res.status(500).json({ message: 'Error processing data', error: (error as Error).message });
+    if (!externalResponse.ok) {
+      throw new Error(`Failed to forward data: ${externalResponse.statusText}`);
     }
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+
+    return NextResponse.json({ status: "Tracked and forwarded successfully" });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
